6c80486c4758deea59939fc11ed82f47b6bf0c11
Dante Cesa <dantec@me.com>
2022-10-07 15:54:38 -0700

    OO: tic tac toe - ruby syntax improvements

--- ruby_basics/Projects/Tic Tac Toe/tic-tac-toe.rb
+++ ruby_basics/Projects/Tic Tac Toe/tic-tac-toe.rb
@@ -1,14 +1,12 @@
 require 'Matrix'
 
 class Player
-    attr_accessor :name
-    attr_reader :mark
-    attr_accessor :games_won
+    attr_accessor :name, :mark, :games_won
     @@count = 0
 
     def initialize(name)
-        @name = name
-        @games_won = 0
+        self.name = name
+        self.games_won = 0
 
         @@count == 0 ? @mark = "X" : @mark = "O"
         @@count += 1
@@ -16,18 +14,16 @@ class Player
 end
 
 class Game
-    attr_reader :players
-    attr_accessor :current_player
-    attr_accessor :num_moves
+    attr_accessor :players, :current_player, :num_moves, :board
 
     def initialize(player1, player2)
-        @players = [player1, player2]
-        @current_player = players[0]
+        self.players = [player1, player2]
+        self.current_player = players[0]
         
-        @board = setup_board
+        self.board = setup_board
     end
 
-    def setup_board()
+    def setup_board
         result = Array.new(3) { Array.new(3) { "-" } }
         
         (1..9).each do |num|
@@ -38,7 +34,7 @@ class Game
         result
     end
 
-    def print_board()
+    def print_board
         puts "\nThe board currently looks like this:"
 
         @board.each_with_index do |line, rows| 
@@ -67,7 +63,7 @@ class Game
         ((input.to_i) - 1).divmod(3)
     end
     
-    def check_for_win()
+    def check_for_win
         rows = Matrix.rows(@board)
         rows.each_with_index do |e, row, col|
             return true if rows.row(row).all?("X") || rows.row(row).all?("O")
@@ -98,25 +94,25 @@ class Game
         while(legal_move == false)
             print "\nPlayer #{current_player.name} where would you like to place an \"#{current_player.mark}\" [0-9]? "
             
-            user_input = gets.chomp().downcase()
+            user_input = gets.chomp.downcase
 
             if (place_input(user_input))
                 if check_for_win == true
                     puts "🎉!! Player #{current_player.name} won!"
         
-                    print_board()
+                    print_board
         
                     current_player.games_won += 1
                     return ask_to_play_again(self)
                 else
-                    toggle_player()
+                    toggle_player
                     legal_move = true
                     @num_moves += 1
                     self
                 end
             else 
                 puts "Illegal move. Please try again."
-                print_board()
+                print_board
                 self
             end
         end
@@ -135,7 +131,7 @@ end
 def ask_to_play_again(current_game)
     old_game = current_game
     print "Do you want to play again [Y/N]? "
-    input = gets.chomp().downcase()
+    input = gets.chomp.downcase
 
     if (input == "y")
         current_game = Game.new(old_game.players[0], old_game.players[1])
@@ -144,12 +140,12 @@ def ask_to_play_again(current_game)
     end
 end
 
-def runtime()
+def runtime
     current_game = nil
 
     while(current_game == nil)
         print "Ready to play [Y/N]? "
-        user_input = gets.chomp().downcase()
+        user_input = gets.chomp.downcase
 
         if (user_input == "n") then
             exit(0)
@@ -157,11 +153,11 @@ def runtime()
 
         if (user_input == "y")
             print "Type a name for player one: "
-            player_one_name = gets.chomp()
+            player_one_name = gets.chomp
             player1 = Player.new(player_one_name)
             
             print "Type a name for player two: "
-            player_two_name = gets.chomp()
+            player_two_name = gets.chomp
             player2 = Player.new(player_two_name)
 
             current_game = Game.new(player1, player2)
@@ -170,7 +166,7 @@ def runtime()
         end
     end
 
-    while (current_game)
+    while current_game
         current_game.print_board
         current_game = current_game.play
         
@@ -182,6 +178,6 @@ def runtime()
 end
 
 puts "Welcome to Tic Tac Toe!"
-runtime()
+runtime
 
 # if you input nil, that'll transcribe as "9"
