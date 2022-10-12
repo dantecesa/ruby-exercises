8cfba77aed0b736357a47fd33f3c779968ed56a4
Dante Cesa <dantec@me.com>
2022-10-12 16:45:42 -0700

    Event Manager (iteration 1 basic CSV parsing)

---
+++ 3-files_and_serialization/projects/event_manager/lib/event_manager.rb
@@ -0,0 +1,10 @@
+puts 'Event Manager Initialized!'
+
+lines = File.readlines('event_attendees.csv')
+
+lines.each_with_index do |line,index|
+  next if index == 0
+  columns = line.split(",")
+  name = columns[2]
+  puts name
+end
