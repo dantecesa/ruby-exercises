e6af255407194821dcab8f2df86dc349744a0b71
Dante Cesa <dantec@me.com>
2022-10-12 16:52:04 -0700

    Event Manager (iteration 2 cleaning up zip codes)

--- 3-files_and_serialization/projects/event_manager/lib/event_manager.rb
+++ 3-files_and_serialization/projects/event_manager/lib/event_manager.rb
@@ -1,9 +1,19 @@
 require 'csv'
 puts 'Event Manager Initialized!'
 
-contents = CSV.open('event_attendees.csv', headers: true)
+contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
 
 contents.each do |row|
-  name = row[2]
-  puts name
-end
+    name = row[:first_name]
+    zipcode = row[:zipcode]
+
+    if zipcode == nil || zipcode.length != 5
+        if zipcode == nil
+            zipcode = "00000"
+        elsif zipcode.length > 5
+            zipcode = zipcode[0..4]
+        end
+    end
+
+    puts "#{name} #{zipcode}"
+  end
