eaaafa2833e5d0f93c4fa0fe01ad921d5c3a4121
Dante Cesa <dantec@me.com>
2022-10-12 17:21:50 -0700

    Event Manager (iteration 4 form letters)

--- 3-files_and_serialization/projects/event_manager/lib/event_manager.rb
+++ 3-files_and_serialization/projects/event_manager/lib/event_manager.rb
@@ -8,7 +8,7 @@ end
 def legislators_by_zip_code(zipcode)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
-  
+
   begin
     legislators = civic_info.representative_info_by_address(
       address: zipcode,
@@ -24,11 +24,16 @@ end
 
 puts 'EventManager initialized.'
 contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
+template_letter = File.read('form_letter.html')
 
 contents.each do |row|
     name = row[:first_name]
     zipcode = clean_zipcode(row[:zipcode])
     legislators = legislators_by_zip_code(zipcode)
 
-    puts "#{name} #{zipcode} #{legislators}"
+    personal_letter = template_letter.gsub('FIRST_NAME', name)
+    personal_letter = personal_letter.gsub('LEGISLATORS', legislators)
+
+    # puts "#{name} #{zipcode} #{legislators}"
+    puts personal_letter
   end
