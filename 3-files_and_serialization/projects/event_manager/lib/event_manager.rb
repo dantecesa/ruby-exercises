d1533e7673107bd6d42b5db1474d6613dc7cbaf1
Dante Cesa <dantec@me.com>
2022-10-12 17:07:39 -0700

    Event Manager (iteration 2 one liner!)

--- 3-files_and_serialization/projects/event_manager/lib/event_manager.rb
+++ 3-files_and_serialization/projects/event_manager/lib/event_manager.rb
@@ -4,15 +4,7 @@ puts 'Event Manager Initialized!'
 contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
 
 def clean_zipcode(zipcode)
-  if zipcode.nil?
-    "00000"
-  elsif zipcode.length > 5
-    zipcode[0..4]
-  elsif zipcode.length < 5
-    zipcode.rjust(5,'0')
-  else
-    zipcode
-  end
+  zipcode.to_s.rjust(5, '0')[0..4]
 end
 
 contents.each do |row|
