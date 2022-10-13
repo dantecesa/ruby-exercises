26a6b9c60d16c3e677e54323989adef5e70f9cb0
Dante Cesa <dantec@me.com>
2022-10-12 17:18:42 -0700

    Event Manager (iteration 3 cleanup)

--- 3-files_and_serialization/projects/event_manager/lib/event_manager.rb
+++ 3-files_and_serialization/projects/event_manager/lib/event_manager.rb
@@ -1,11 +1,25 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
-
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zip_code(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+  
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislator_names = legislators.officials.map { |legislator| legislator.name}
+    legislator_string = legislator_names.join(", ")
+  rescue => exception
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
 end
 
 puts 'EventManager initialized.'
@@ -14,17 +28,7 @@ contents = CSV.open('event_attendees.csv', headers: true, header_converters: :sy
 contents.each do |row|
     name = row[:first_name]
     zipcode = clean_zipcode(row[:zipcode])
-
-    begin
-      legislators = civic_info.representative_info_by_address(
-        address: zipcode,
-        levels: 'country',
-        roles: ['legislatorUpperBody', 'legislatorLowerBody']
-      )
-      legislators = legislators.officials    
-    rescue => exception
-      'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
-    end
+    legislators = legislators_by_zip_code(zipcode)
 
     puts "#{name} #{zipcode} #{legislators}"
   end
