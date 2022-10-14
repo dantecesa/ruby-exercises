72a3795e6472d2d14b15ff681ae58568eb2fb58c
Dante Cesa <dantec@me.com>
2022-10-14 13:22:00 -0700

    Event Manager assignment: clean up phone numbers

--- 3-files_and_serialization/projects/event_manager/lib/event_manager.rb
+++ 3-files_and_serialization/projects/event_manager/lib/event_manager.rb
@@ -6,6 +6,33 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+def clean_phone_number(phone_number)
+  phone = phone_number
+
+  if phone.length == 11
+    if phone[0] == '1'
+      phone = phone[1..]
+    end
+  end
+
+  if phone.length != 10
+    phone = phone.gsub(" ", "")
+    phone = phone.gsub("-", "")
+    phone = phone.gsub(".", "")
+    phone = phone.gsub("(", "")
+    phone = phone.gsub(")", "")
+  end
+
+  if phone.length == 10
+    phone = "(#{phone[0..2]}) #{phone[3..5]}-#{phone[6..9]}"
+  end
+
+  if phone.length != 14
+    phone = "Bad number!"
+  end
+  phone
+end
+
 def legislators_by_zip_code(zipcode)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -39,10 +66,12 @@ erb_template = ERB.new template_letter
 contents.each do |row|
     id = row[0]
     name = row[:first_name]
+    phone = clean_phone_number(row[:homephone])
     zipcode = clean_zipcode(row[:zipcode])
-    legislators = legislators_by_zip_code(zipcode)
 
-    form_letter = erb_template.result(binding)
+    puts "#{phone} - #{zipcode}: #{name}"
 
-    save_thank_you_letter(id, form_letter)
+    # legislators = legislators_by_zip_code(zipcode)
+    # form_letter = erb_template.result(binding)
+    # save_thank_you_letter(id, form_letter)
   end
